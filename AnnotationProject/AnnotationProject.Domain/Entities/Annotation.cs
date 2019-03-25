44f81cc1f8ef8537d3837584e6b41e5b2122374d
Breno Ramos <ramosb317@gmail.com>
2019-03-25 17:35:52 -0300

    criação do domínio Anotação.

---
+++ AnnotationProject/AnnotationProject.Domain/Entities/Annotation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AnnotationProject.Shared.Entities;
+
+namespace AnnotationProject.Domain.Entities
+{
+    public class Annotation : Entity
+    {
+        public string Name { get; private set; }        
+        public string Description { get; private set; }
+        public string User { get; private set; }
+        public DateTime InsertDate { get; private set; }
+        public DateTime? UpdateDate { get; private set; }
+
+        public Annotation(string name, string description, string user)
+        {
+            Name = name;
+            Description = description;
+            User = user;
+            InsertDate = DateTime.Now;
+        }
+
+        public void Update(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            UpdateDate = DateTime.Now;
+        }
+    }
+}
---
+++ AnnotationProject/AnnotationProject.Shared/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnnotationProject.Shared.Entities
+{
+    public class Entity
+    {
+        public string Id { get; private set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid().ToString().Replace("-", "");
+        }
+    }
+}
